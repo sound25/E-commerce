f95398818ba08670695946cd972093fb7c3ad99f
Soundarya <34751165+sound25@users.noreply.github.com>
2021-10-30 13:05:05 +0530

    Adding search functionality
    Adding search functionality

--- ecom/shop/views.py
+++ ecom/shop/views.py
@@ -3,4 +3,10 @@ from .models import Products
 # Create your views here.
 def index(request):
     product_objects=Products.objects.all()
+    item_name=request.GET.get('item_name')
+    if item_name != '' and item_name is not None:
+        product_objects=Products.objects.filter(title__icontains=item_name)
     return render(request, 'shop/index.html',{'product_objects':product_objects})
+
+
+
